45fa18761a62fb311040c03868f49f2289b91b17
te4asatoryu <te3a180598@gmail.com>
2021-12-01 17:02:22 +0900

    何もわからん

---
+++ app/src/main/java/com/example/flrs/PageConfiguration.kt
@@ -0,0 +1,11 @@
+package com.example.flrs
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PageConfiguration : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_page_configuration)
+    }
+}
