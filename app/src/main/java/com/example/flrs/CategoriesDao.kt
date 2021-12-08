2d9165c49d55d712963bc151dcf1497e7774aef6
te4asatoryu <te3a180598@gmail.com>
2021-12-08 17:53:12 +0900

    ryuta bunta 居残りcommit

--- app/src/main/java/com/example/flrs/CategoriesDao.kt
+++ app/src/main/java/com/example/flrs/CategoriesDao.kt
@@ -5,13 +5,13 @@ import androidx.room.Dao
 
 @Dao
 interface CategoriesDao {
-    @Query("SELECT * FROM Categories")
-    fun getAll():List<RowModel>
-
-    @Insert
-    fun insert(category: Categories)
-    @Update
-    fun update(category: Categories)
-    @Delete
-    fun delete(category: Categories)
+//    @Query("SELECT * FROM Categories")
+//    fun getAll():List<RowModel>
+//
+//    @Insert
+//    fun insert(category: Categories)
+//    @Update
+//    fun update(category: Categories)
+//    @Delete
+//    fun delete(category: Categories)
 }
