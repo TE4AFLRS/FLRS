d7b70b24658eb3f0b46d087f6435f7de38511876
te4asatoryu <te3a180598@gmail.com>
2021-12-13 09:21:28 +0900

    bunta12/13

--- app/src/main/java/com/example/flrs/RowModel.kt
+++ app/src/main/java/com/example/flrs/RowModel.kt
@@ -3,6 +3,7 @@ package com.example.flrs
 class RowModel {
     var food_id:String = ""
     var food_name:String = ""
+    var period:String = ""
     var register_date:String= ""
 //    var limit_date:String?=""
 }
