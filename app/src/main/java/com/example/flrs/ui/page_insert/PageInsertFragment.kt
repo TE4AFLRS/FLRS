52362fc098d0e267fd91abffd18fa2a49f1bd461
中島 文汰 <180009@jc-21.jp>
2021-12-15 10:27:40 +0900

    insert_button

--- app/src/main/java/com/example/flrs/ui/page_insert/PageInsertFragment.kt
+++ app/src/main/java/com/example/flrs/ui/page_insert/PageInsertFragment.kt
@@ -3,16 +3,11 @@ package com.example.flrs.ui.page_insert
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.*
 import androidx.activity.addCallback
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.commit
 import com.example.flrs.R
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-import java.util.*
 
 class PageInsertFragment : Fragment(R.layout.fragment_page_insert) {
 
@@ -31,20 +26,20 @@ class PageInsertFragment : Fragment(R.layout.fragment_page_insert) {
 
         }
 
-        val linearLayout = view.findViewById<View>(R.id.scrollView2) as LinearLayout
+        val vg = view.findViewById<View>(R.id.insert_table) as ViewGroup
         val fab: FloatingActionButton = view.findViewById(R.id.insert_fab)
-        layoutInflater.inflate(R.layout.page_insert_table_row, linearLayout)
+        layoutInflater.inflate(R.layout.page_insert_table_row, vg)
         // 行を追加
         fab.setOnClickListener {
-            layoutInflater.inflate(R.layout.page_insert_table_row, linearLayout)
-            var button = ImageButton(context)
-            var id: Int = View.generateViewId()
-            button.id = id
-            print(button.id)
-            val tr = linearLayout.getChildAt(id) as TableRow;
+            layoutInflater.inflate(R.layout.page_insert_table_row, vg)
+//            var button = ImageButton(context)
+//            var id: Int = View.generateViewId()
+//            button.id = id
+//            print(button.id)
+            val tr = vg.getChildAt(id) as TableRow;
             (tr.getChildAt(1) as ImageButton)?.setOnClickListener {
                 // buttonをクリックした際の処理を記述
-                linearLayout.removeView()
+                vg.removeView()
 
             }
         }
