5f469052436e2a67441d46d954e8554bc48a0af5
中島 文汰 <180009@jc-21.jp>
2021-12-15 15:44:26 +0900

    spinner実装

--- app/src/main/java/com/example/flrs/ui/page_insert/PageInsertFragment.kt
+++ app/src/main/java/com/example/flrs/ui/page_insert/PageInsertFragment.kt
@@ -2,6 +2,7 @@ package com.example.flrs.ui.page_insert
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.View
 import android.view.ViewGroup
@@ -10,10 +11,7 @@ import android.widget.ImageButton
 import android.widget.LinearLayout
 import android.widget.Spinner
 import androidx.activity.addCallback
-import com.example.flrs.Categories
-import com.example.flrs.CategoriesDao
-import com.example.flrs.CategoryRow
-import com.example.flrs.R
+import com.example.flrs.*
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class PageInsertFragment : Fragment(R.layout.fragment_page_insert) {
@@ -32,26 +30,30 @@ class PageInsertFragment : Fragment(R.layout.fragment_page_insert) {
         requireActivity().onBackPressedDispatcher.addCallback(this) {
             parentFragmentManager.popBackStack()
         }
+        mCategoriesDao =FoodsDatabase.getInstance(requireContext()).categoriesDao()
         var list:List<CategoryRow> = mCategoriesDao.getAll()
-        var nameList:List<String>
-        for (i in list){
-            nameList = list
-        }
+//        var nameList:List<String>
+//        Log.d("DEBUG",list.toString())
+//        for (i in list){
+//            nameList = list.get((i),(1))
+//        }
 
-        val addpter = ArrayAdapter(requireContext(),android.R.layout.simple_spinner_item,list)
+        val adapter = ArrayAdapter(requireContext(),android.R.layout.simple_spinner_item,list.map{it.category_name})
 
 
-        val spinner:Spinner = view.findViewById(R.id.spinner)
 
-        spinner.adapter = addpter
 
         val vg = view.findViewById<View>(R.id.insert_table) as ViewGroup
         val fab: FloatingActionButton = view.findViewById(R.id.insert_fab)
         layoutInflater.inflate(R.layout.page_insert_table_row, vg)
+        val spinner:Spinner = view.findViewById(R.id.spinner)
+        spinner.adapter = adapter
         // 行を追加
         fab.setOnClickListener {
-            val view = layoutInflater.inflate(R.layout.page_insert_table_row, vg) as LinearLayout
-            val row = (view.getChildAt(view.childCount - 1) as LinearLayout)
+            val table = layoutInflater.inflate(R.layout.page_insert_table_row, vg) as LinearLayout
+            val row = (table.getChildAt(table.childCount - 1) as LinearLayout)
+            val spinner:Spinner = row.findViewById(R.id.spinner)
+            spinner.adapter = adapter
             (row.getChildAt(0)  as ImageButton)?.setOnClickListener{
                 vg.removeView(row)
             }
