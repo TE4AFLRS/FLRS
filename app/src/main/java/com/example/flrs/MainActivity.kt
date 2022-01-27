1de6c383d01c4b4bbbef410c8ac50938c1c013a7
中島 文汰 <180009@jc-21.jp>
2022-01-27 16:23:52 +0900

    buntacommit

--- app/src/main/java/com/example/flrs/MainActivity.kt
+++ app/src/main/java/com/example/flrs/MainActivity.kt
@@ -6,36 +6,23 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.graphics.BitmapFactory
-import android.os.Build
 import android.os.Build.*
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.addCallback
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var contextDelivery: Context
-    private lateinit var manager: NotificationManager
-
-    fun getContext():Context{
-        return contextDelivery
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        contextDelivery = applicationContext
 
         // Android 8 (Oreo) への対応
         if (VERSION.SDK_INT >= VERSION_CODES.O) {
@@ -46,24 +33,26 @@ class MainActivity : AppCompatActivity() {
             manager.createNotificationChannel(channel)
         }
 
-//        contextDelivery = applicationContext
-
         //alarmManagerの実装
         val alarmManager =
             this.getSystemService(Context.ALARM_SERVICE) as? AlarmManager
 
         // プッシュ通知を表示
         val intent = Intent(this, NotificationProcess::class.java)
-        val contentIntent =PendingIntent.getBroadcast(this, 0, intent, 0)
-        Toast.makeText(this, "Alarm set", Toast.LENGTH_SHORT).show()
-        alarmManager?.setExact(AlarmManager.RTC_WAKEUP, System.currentTimeMillis()+2000, contentIntent)
+        val contentIntent = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
+        val c: Calendar = Calendar.getInstance()
+        c.set(Calendar.HOUR_OF_DAY, 6)
+        alarmManager?.setRepeating(
+            AlarmManager.RTC_WAKEUP,
+            c.timeInMillis,
+            AlarmManager.INTERVAL_HALF_DAY,
+            contentIntent
+        )
 
-        //
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
         val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.navigation_home,
@@ -80,6 +69,4 @@ class MainActivity : AppCompatActivity() {
             moveTaskToBack(true)
         }
     }
-
-
 }
